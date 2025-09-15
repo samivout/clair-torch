2b06d532c9f327f24c9d596a598fba13142d40a5
samivout <124705587+samivout@users.noreply.github.com>
2025-09-15 10:02:45 +0300

    Added handling for new dark_field_dataset.

--- clair_torch/inference/hdr_merge.py
+++ clair_torch/inference/hdr_merge.py
@@ -8,7 +8,7 @@ import torch
 from torch.utils.data import DataLoader
 
 from clair_torch.common import general_functions as gf, transforms as tr
-from clair_torch.datasets.image_dataset import ArtefactMapDataset
+from clair_torch.datasets.image_dataset import FlatFieldArtefactMapDataset, DarkFieldArtefactMapDataset, ImageMapDataset
 from clair_torch.models.base import ICRFModelBase
 from clair_torch.training.losses import gaussian_value_weights
 from clair_torch.common.statistics import WBOMean
@@ -16,29 +16,32 @@ from clair_torch.common.statistics import WBOMean
 
 def compute_hdr_image(dataloader: DataLoader, device: str | torch.device,
                       icrf_model: Optional[ICRFModelBase] = None, weight_fn: Optional[Callable] = None,
-                      flatfield_dataset: Optional[ArtefactMapDataset] = None,
-                      gpu_transforms: Optional[tr.Transform | Sequence[tr.Transform]] = None) \
+                      flat_field_dataset: Optional[FlatFieldArtefactMapDataset] = None,
+                      gpu_transforms: Optional[tr.BaseTransform | Sequence[tr.BaseTransform]] = None,
+                      dark_field_dataset: Optional[DarkFieldArtefactMapDataset] = None) \
         -> tuple[torch.Tensor, torch.Tensor | None]:
     """
     Function for computing HDR merging of a set of images at different exposure times under stationary conditions.
     Uncertainty can be computed if the dataset is provided with PairedFrameSettings instances that include a path
-    to an uncertainty image. Flatfield correction can be performed if an ArtefactMapDataset is provided and a matching
-    artefact image is found. Uncertainty of the flatfield correction is computed on similar conditions as to the main
-    image_dataset.
+    to an uncertainty image. Flat field correction can be performed if an FlatFieldArtefactMapDataset is provided and a
+    matching artefact image is found. Similarly, a dark field correction can be performed with a
+    DarkFieldArtefactMapDataset.
     Args:
         dataloader: DataLoader containing an ImageMapDataset instance, which should contain FrameSettings or
             PairedFrameSettings instances.
         device: the device to run the computations on.
         icrf_model: an optional ICRF model to use to linearize the images before merging.
         weight_fn: a weighting function that takes as input the batch of images.
-        flatfield_dataset: an ArtefactMapDataset for flatfield correction.
+        flat_field_dataset: a FlatFieldArtefactMapDataset for flat field correction.
         gpu_transforms: Optional transform operations to be performed on the image batch after moving the data to the
             desired device.
+        dark_field_dataset: a DarkFieldArtefactMapDataset for dark field correction.
 
     Returns:
         A tuple of tensors representing (the HDR image, uncertainty of the HDR image or None).
     """
     expected_number_of_iterations = len(dataloader)
+    main_dataset: ImageMapDataset = dataloader.dataset
 
     if isinstance(gpu_transforms, Iterable):
         gpu_transforms = gpu_transforms
@@ -51,7 +54,7 @@ def compute_hdr_image(dataloader: DataLoader, device: str | torch.device,
     mean_handler = WBOMean(dim=0)
 
     # HDR val and std image computations.
-    for val_batch, std_batch, meta_batch in tqdm(dataloader, desc="Batches processed", total=expected_number_of_iterations):
+    for index_batch, val_batch, std_batch, meta_batch in tqdm(dataloader, desc="Batches processed", total=expected_number_of_iterations):
 
         # Stage tensors on correct device.
         images = val_batch.to(device=device)
@@ -60,10 +63,29 @@ def compute_hdr_image(dataloader: DataLoader, device: str | torch.device,
 
         # Run GPU transforms.
         for transform in gpu_transforms:
-            images = transform(images)
+            if transform is not None:
+                images = transform(images)
 
         # Set gradient requirements.
         images.requires_grad_(stds is not None)
+
+        dark_field_val, dark_field_std = None, None
+        if dark_field_dataset is not None:
+
+            frame_settings_in_batch = []
+            for index in index_batch:
+                frame_settings_in_batch.append(main_dataset.files[index])
+
+            _, dark_field_val, dark_field_std, _ = dark_field_dataset.get_matching_artefact_images(frame_settings_in_batch)
+
+            if dark_field_val is not None:
+                dark_field_val, dark_field_std = dark_field_val.to(device=device), dark_field_std.to(device=device)
+
+                dark_field_val.requires_grad_(dark_field_std is not None)
+
+                with torch.set_grad_enabled(dark_field_std is not None):
+                    images = gf.conditional_gaussian_blur(images, dark_field_val, threshold=0.05, kernel_size=3,
+                                                          differentiable=True)
 
         # Compute weights
         batch_weight = torch.ones_like(images) if weight_fn is None else gaussian_value_weights(images)
@@ -83,17 +105,28 @@ def compute_hdr_image(dataloader: DataLoader, device: str | torch.device,
                 outputs=running_average,
                 inputs=images,
                 grad_outputs=torch.ones_like(running_average),
-                retain_graph=False
+                retain_graph=True
             )[0]
             variance_update = torch.sum((running_gradient * stds) ** 2, dim=0, keepdim=True)
             running_variance = variance_update if running_variance is None else running_variance + variance_update
 
+        if dark_field_std is not None:
+            running_gradient = torch.autograd.grad(
+                outputs=running_average,
+                inputs=dark_field_val,
+                grad_outputs=torch.ones_like(running_average),
+                retain_graph=False
+            )[0]
+
+            variance_update = torch.sum((running_gradient * dark_field_std) ** 2, dim=0, keepdim=True)
+            running_variance = running_variance + variance_update
+
         mean_handler.internal_detach()
 
     # Flatfield corrections.
-    if flatfield_dataset is not None:
+    if flat_field_dataset is not None:
 
-        flatfield_val, flatfield_std, _ = flatfield_dataset.get_matching_artefact_image(dataloader.dataset.files[0], matching_attributes=["magnification, illumination"])
+        _, flatfield_val, flatfield_std, _ = flat_field_dataset.get_matching_artefact_images([main_dataset.files[0]])
         flatfield_val, flatfield_std = flatfield_val.to(device=device), flatfield_std.to(device=device)
 
         if flatfield_std is not None:
@@ -104,14 +137,14 @@ def compute_hdr_image(dataloader: DataLoader, device: str | torch.device,
         flatfield_corrected_running_average = gf.flatfield_correction(running_average, flatfield_val, flatfield_mean)
 
         if flatfield_std is not None:
-            flatfield_grad = torch.autograd.grad(
+            running_gradient = torch.autograd.grad(
                 outputs=flatfield_corrected_running_average,
                 inputs=flatfield_val,
                 grad_outputs=torch.ones_like(flatfield_corrected_running_average),
                 retain_graph=False
             )[0]
 
-            running_variance = running_variance + (flatfield_grad * flatfield_std) ** 2
+            running_variance = running_variance + (running_gradient * flatfield_std) ** 2
 
         running_average = flatfield_corrected_running_average
 
