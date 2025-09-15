5e6280686866b58b923968b991ed0bb5a829f227
samivout <124705587+samivout@users.noreply.github.com>
2025-09-15 10:16:20 +0300

    Modified main argument to accept a Dataloader instead of Dataset.

--- clair_torch/inference/measure_linearity.py
+++ clair_torch/inference/measure_linearity.py
@@ -9,12 +9,10 @@ from torch.utils.data import DataLoader
 
 from clair_torch.training.losses import combined_gaussian_pair_weights, pixelwise_linearity_loss, compute_spatial_linearity_loss
 from clair_torch.common.general_functions import get_valid_exposure_pairs, get_pairwise_valid_pixel_mask
-from datasets.collate import custom_collate
-from clair_torch.datasets.image_dataset import ImageMapDataset
-from models import ICRFModelBase
+from clair_torch.models.base import ICRFModelBase
 
 
-def measure_linearity(image_dataset: ImageMapDataset, device: str | torch.device,
+def measure_linearity(dataloader: DataLoader, device: str | torch.device,
                       use_uncertainty_weighting: bool = True, use_relative_linearity_loss: bool = True,
                       icrf_model: Optional[ICRFModelBase] = None) \
         -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor | None]:
@@ -25,7 +23,7 @@ def measure_linearity(image_dataset: ImageMapDataset, device: str | torch.device
     linearity losses, and the uncertainty of the spatial linearity losses. By leaving the ICRF model None, you can
     get a measure of the camera's linearity as it is on its own.
     Args:
-        image_dataset: an ImageMapDataset consisting of FrameSettings or PairedFrameSettings objects.
+        dataloader: an ImageMapDataset consisting of FrameSettings or PairedFrameSettings objects.
         device: the device to run the computation on.
         use_uncertainty_weighting: whether to utilize inverse uncertainty weights or not.
         use_relative_linearity_loss: whether to utilize relative or absolute values for linearity loss computation.
@@ -35,32 +33,24 @@ def measure_linearity(image_dataset: ImageMapDataset, device: str | torch.device
         A tuple of tensors representing (exposure time ratios, spatial linearity losses,
         standard deviation of spatial linearity losses, uncertainty of the spatial linearity losses).
     """
-    dataloader = DataLoader(dataset=image_dataset, batch_size=13, shuffle=False, drop_last=False,
-                            pin_memory=False, collate_fn=custom_collate)
 
-    for val_batch, std_batch, meta_batch in tqdm(dataloader, desc="Creating linearity plot"):
+    for index_batch, val_batch, std_batch, meta_batch in tqdm(dataloader, desc="Creating linearity plot"):
 
         images = val_batch.to(device=device)
-        if std_batch is not None:
-            stds = std_batch.to(device=device)
-        else:
-            stds = None
+        stds = std_batch.to(device=device) if std_batch is not None else None
         exposures = meta_batch['exposure_time'].to(device=device)
 
         i_idx, j_idx, ratio_pairs = get_valid_exposure_pairs(increasing_exposure_values=exposures, exposure_ratio_threshold=0.2)
-        valid_mask = get_pairwise_valid_pixel_mask(images, i_idx, j_idx, stds, lower=1 / 255, upper=254 / 255)
+        valid_mask = get_pairwise_valid_pixel_mask(images, i_idx, j_idx, stds, val_lower=1 / 255, val_upper=254 / 255)
         gaussian_weight = combined_gaussian_pair_weights(images, i_idx, j_idx)
 
-        if stds is not None:
-            images.requires_grad_(True)
-        else:
-            images.requires_grad_(False)
+        # Set gradient requirements.
+        images.requires_grad_(stds is not None)
 
-        if icrf_model is not None:
-            linearized = icrf_model(images)  # Shape: (N, C, H, W)
-        else:
-            linearized = val_batch
+        # Linearize if an ICRF model is provided.
+        linearized = icrf_model(images) if icrf_model is not None else images
 
+        # Compute uncertainty of linearization if uncertainty images are available.
         if stds is not None:
             grads = torch.autograd.grad(
                 outputs=linearized,
