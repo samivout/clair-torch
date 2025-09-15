ac7de678d84fe9063a45b96dd35b20c2d854ff17
samivout <124705587+samivout@users.noreply.github.com>
2025-09-15 10:03:58 +0300

    Changed input argument from a Dataset to a Dataloader.

--- clair_torch/inference/inferential_statistics.py
+++ clair_torch/inference/inferential_statistics.py
@@ -11,33 +11,23 @@ from torch.utils.data import DataLoader
 
 from clair_torch.common.enums import VarianceMode
 from clair_torch.common.statistics import WBOMeanVar
-from clair_torch.datasets.video_frame_dataset import VideoIterableDataset
-from datasets.collate import custom_collate
-from models import ICRFModelBase
+from clair_torch.models.base import ICRFModelBase
 
 
-def compute_video_mean_and_std(video_dataset: VideoIterableDataset, batch_size: int, device: str | torch.device,
+def compute_video_mean_and_std(dataloader: DataLoader, device: str | torch.device,
                                icrf_model: Optional[ICRFModelBase] = None) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Function for computing the mean and standard deviation of the frames in a given dataset of video files. All frames
     in all the videos are treated as belonging in the same dataset.
     Args:
-        video_dataset: a VideoIterableDataset representing the dataset.
-        batch_size: how many frames from the videos to utilize in a single batch.
+        dataloader: a DataLoader containing a VideoIterableDataset, representing the dataset.
         device: torch device to run the computation on.
         icrf_model: an ICRF model to optionally linearize the pixel values before computing the mean and std.
 
     Returns:
         The mean and standard deviations of the (possibly linearized) video frames in the dataset.
     """
-    total_number_of_frames = len(video_dataset)
-    total_iterations = math.ceil(total_number_of_frames / batch_size)
-
-    dataloader = DataLoader(video_dataset, batch_size=batch_size, shuffle=False, drop_last=False, pin_memory=False,
-                            collate_fn=custom_collate)
-
-    if icrf_model is not None:
-        icrf_model = icrf_model.to(device=device)
+    total_iterations = len(dataloader)
 
     mean_handler = WBOMeanVar(dim=0, variance_mode=VarianceMode.SAMPLE_FREQUENCY)
     number_of_frames = 0
