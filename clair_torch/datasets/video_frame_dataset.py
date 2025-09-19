78fadb65525ff2e3a7eb22fb14fb274f9eeef6c6
samivout <124705587+samivout@users.noreply.github.com>
2025-09-19 13:39:50 +0300

    Add missing type hints to VideoIterableDataset.__init__

--- clair_torch/datasets/video_frame_dataset.py
+++ clair_torch/datasets/video_frame_dataset.py
@@ -1,9 +1,10 @@
 """
 Module for the video related dataset class, which inherits the IterableDataset from torch.utils.data.
 """
-from typing import List, Tuple
 
-from clair_torch.common.file_settings import FrameSettings
+from typeguard import typechecked
+
+from clair_torch.common.file_settings import FrameSettings, PairedFrameSettings
 from clair_torch.common.enums import MissingStdMode
 from clair_torch.common.data_io import load_video_frames_generator
 
@@ -11,8 +12,9 @@ import torch
 
 
 class VideoIterableDataset(torch.utils.data.IterableDataset):
-    def __init__(self, frame_settings: List[FrameSettings], missing_std_mode=MissingStdMode.CONSTANT,
-                 missing_std_value=0.0):
+    @typechecked
+    def __init__(self, frame_settings: tuple[FrameSettings | PairedFrameSettings, ...],
+                 missing_std_mode: MissingStdMode = MissingStdMode.CONSTANT, missing_std_value: float = 0.0):
         """
         # TODO: add handling for std files along the main value files.
         Dataset class for video files. Treats all encompassed files as a single dataset, jumping smoothly from one file
@@ -35,7 +37,7 @@ class VideoIterableDataset(torch.utils.data.IterableDataset):
             number_of_frames += frame_setting.get_numeric_metadata()["number_of_frames"]
         return number_of_frames
 
-    def __iter__(self) -> Tuple[int, torch.Tensor, torch.Tensor | None, dict[str, float | int]]:
+    def __iter__(self) -> tuple[int, torch.Tensor, torch.Tensor | None, dict[str, float | int]]:
         """
         Access method for the frames of this dataset. Iterates through the files and frames, moving on to the next file
         upon exhausting a file.
@@ -47,13 +49,13 @@ class VideoIterableDataset(torch.utils.data.IterableDataset):
             input_paths = settings.get_input_paths()
             transforms = settings.get_transforms()
 
-            if isinstance(input_paths, Tuple):
+            if isinstance(input_paths, tuple):
                 val_path, std_path = input_paths
             else:
                 val_path = input_paths
                 std_path = None
 
-            if isinstance(transforms, Tuple):
+            if isinstance(transforms, tuple):
                 val_transforms, std_transforms = transforms
             else:
                 val_transforms = transforms
@@ -61,15 +63,19 @@ class VideoIterableDataset(torch.utils.data.IterableDataset):
 
             metadata = settings.get_numeric_metadata()
 
-            for frame in load_video_frames_generator(val_path, val_transforms):
-                if self.missing_std_mode == MissingStdMode.NONE:
-                    std_image = None
-                elif self.missing_std_mode == MissingStdMode.CONSTANT:
-                    std_image = self.shared_std_tensor.expand_as(frame)
-                elif self.missing_std_mode == MissingStdMode.MULTIPLIER:
-                    std_image = frame * self.shared_std_tensor
-                else:
-                    raise ValueError(f"Unsupported MissingStdMode: {self.missing_std_mode}")
+            try:
+                for frame in load_video_frames_generator(val_path, val_transforms):
+                    if self.missing_std_mode == MissingStdMode.NONE:
+                        std_image = None
+                    elif self.missing_std_mode == MissingStdMode.CONSTANT:
+                        std_image = self.shared_std_tensor.expand_as(frame)
+                    elif self.missing_std_mode == MissingStdMode.MULTIPLIER:
+                        std_image = frame * self.shared_std_tensor
+                    else:
+                        raise ValueError(f"Unsupported MissingStdMode: {self.missing_std_mode}")
 
-                self._running_index += 1
-                yield self._running_index, frame, std_image, metadata
+                    self._running_index += 1
+                    yield self._running_index - 1, frame, std_image, metadata
+            except StopIteration:
+                return
+        return
